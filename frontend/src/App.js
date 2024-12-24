a8be062024b42e1cae2eb223fedf3cd93266dd37
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2024-12-24 19:26:34 +0530

    profile page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import Explore from "./components/Explore";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="h-screen">
       {/* <Home /> */}
-      <Explore />
+      {/* <Explore /> */}
+      <Profile />
     </div>
   );
 }
